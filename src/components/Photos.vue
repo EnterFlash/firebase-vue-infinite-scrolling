a87e475465fa727fdde9ce7978b312bd3ac4befa
Carlos Eli Lopez Tellez <carloselilopezt@gmail.com>
2021-01-21 09:12:00 -0600

    At the start of the tutorial

---
+++ src/components/Photos.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h1 class="mt-3">Photos Gallery</h1>
+          <div>My photos go here! 😮</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Photos',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
